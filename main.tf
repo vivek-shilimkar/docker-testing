6535c9ca6020a83b8ffb836c7ee1326bf91f6fa1
vivek-infracloud <vivek.shilimkar@infracloud.io>
2025-04-21 11:04:29 +0530

    docker testing

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     http = {
-      source  = "Mastercard/http"
-      version = "~> 1.2.0"
+      source  = "paultyng/http"
+      version = "~> 2.0"
     }
   }
 }
